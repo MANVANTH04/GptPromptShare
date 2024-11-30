468f3359511b31e3885e9074c0e3aed7ee85d463
[manvanth] <manvanth.me@gmail.com>
2024-11-30 23:47:51 +0530

    commit

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Homelayout = () => {
+  return (
+    <html>
+    <head>
+
+    </head>
+        <body>
+
+        <div> home layout </div>
+            
+        </body>
+    </html>
+
+   
+  
+ )
+}
+
+export default Homelayout
