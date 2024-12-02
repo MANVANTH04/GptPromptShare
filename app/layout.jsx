c3b078ce12cd8c147d7447e323265b3770329e60
[manvanth] <manvanth.me@gmail.com>
2024-12-02 23:53:02 +0530

    s

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,6 +14,12 @@ const Homelayout = () => {
                 <h1>One place for all your prompts. </h1>
                 <p>Share your gpt prompts. </p>
             </div>
+
+            <div>
+                <h1>One place for all your prompts. </h1>
+                <p>Share your gpt prompts. </p>
+            </div>
+            
         </body>
     </html>
 
